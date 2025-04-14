70e8216660d0a6684828be770bfea915cb3f20bf
watsonation <35076820+awatson246@users.noreply.github.com>
2025-04-14 15:22:32 -0400

    Updated plc types, fixed typos

--- src/pycom_example.py
+++ src/pycom_example.py
@@ -6,20 +6,20 @@ first_plc.open()
 state = 0
 
 while True: 
-	with LogixDriver('192.168.___.___') as micro850:
+	with LogixDriver('192.168.___.___') as micro870:
 		print(micro850)
 		
 		# System off condition
 		while state == 0:
 			print(state)
 			
-			taglist = micro850.read('_IO_EM_DI_01')
+			taglist = micro870.read('_IO_EM_DI_01')
 			green = taglist[1]
-			taglist = micro850.read('_IO_EM_DI_00')
+			taglist = micro870.read('_IO_EM_DI_00')
 			red = taglist[1]
 			
-			micro850.write('_IO_EM_DO_07', green)
-			micro850.write('IO_EM_DO_06', red)
+			micro870.write('_IO_EM_DO_07', green)
+			micro870.write('IO_EM_DO_06', red)
 			
 			time.sleep(0.1)
 			
@@ -27,20 +27,20 @@ while True:
 				# Move to system on
 				state = 1
 				while green:
-					taglist = micro850.read('_IO_EM_DI_01')
-					freen = taglist[1]
+					taglist = micro870.read('_IO_EM_DI_01')
+					green = taglist[1]
 					
 		# System on condition
 		while state == 1:
 			print(state)
 			
-			taglist = micro850.read('_IO_EM_DI_01')
+			taglist = micro870.read('_IO_EM_DI_01')
 			green = taglist[1]
-			taglist = micro850.read('_IO_EM_DI_00')
+			taglist = micro870.read('_IO_EM_DI_00')
 			red = taglist[1]
 			
-			micro850.write('_IO_EM_DO_07', green)
-			micro850.write('IO_EM_DO_06', red)
+			micro870.write('_IO_EM_DO_07', green)
+			micro870.write('IO_EM_DO_06', red)
 			
 			time.sleep(0.1)
 			
@@ -49,5 +49,5 @@ while True:
 				# Move to system off
 				state = 0
 				while green:
-					taglist = micro850.read('_IO_EM_DI_01')
+					taglist = micro870.read('_IO_EM_DI_01')
 					freen = taglist[1]
